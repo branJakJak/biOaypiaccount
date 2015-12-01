eff4b832d25f4ab9f912673c34bb9171097f8e3f
kebyang <kelvo_link@yahoo.com>
2015-12-02 00:07:51 +0800

    updated console db confirmat

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -28,8 +28,8 @@ class SiteController extends Controller
 	public function actionIndex()
 	{
 		// renders the view file 'protected/views/site/index.php'
-		// using the default layout 'protected/views/layouts/main.php'
-		$this->render('index');
+		$this->redirect(array('/subAccount/create'));
+		// $this->render('index');
 	}
 
 	public function actionTest()
