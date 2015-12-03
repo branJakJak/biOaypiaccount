73e5ae974a1123eaa34736dee21b6f4165876112
kebyang <kelvo_link@yahoo.com>
2015-12-03 23:18:02 +0800

    main accoun checfker

--- themes/abound/views/layouts/main.php
+++ themes/abound/views/layouts/main.php
@@ -4,9 +4,8 @@
     <meta charset="utf-8">
     <title><?php echo Yii::app()->name ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Free yii themes, free web application theme">
-    <meta name="author" content="Webapplicationthemes.com">
-	<link href='http://fonts.googleapis.com/css?family=Carrois+Gothic' rel='stylesheet' type='text/css'>
+
+	 <link href='http://fonts.googleapis.com/css?family=Carrois+Gothic' rel='stylesheet' type='text/css'>
 
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
