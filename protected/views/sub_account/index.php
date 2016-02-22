bb4285b9a78d1772e6ea2d9378f1f29fa5ef93a9
Your Name <you@example.com>
2016-02-23 03:28:23 +0800

    suma sama sa scroll

--- protected/views/sub_account/index.php
+++ protected/views/sub_account/index.php
@@ -69,77 +69,79 @@
 				</li>
 		    </ul>
 		</div>
-		<div class="span9 sub-account-main-container">
-			<div class='well' ng-show="selectedMainAccountKey === null">
-				<h1>
-					Sub Account Panel
-				</h1>
-			</div>
-			<div ng-show="selectedMainAccountKey !== null">
-				<div class="well">
-					<h3>
-						<small>*You can click the link to copy to Clipboard</small> <br>
-						Main Account : 
-						<button title="click to copy to clipboard" clipboard text="selectedMainAccount.username" type='button' class='btn btn-link'>
-							{{selectedMainAccount.username}}
-						</button>
-						<button title="click to copy to clipboard" clipboard text="selectedMainAccount.password" type='button' class='btn btn-link'>
-							{{selectedMainAccount.password}}
-						</button>
-						
-					</h3>
+		<div class="span9" >
+			<div data-offset-top="100" data-spy="affix" class=" sub-account-main-container span9">
+				<div class='well' ng-show="selectedMainAccountKey === null">
+					<h1>
+						Sub Account Panel
+					</h1>
 				</div>
-				<p>
-                <input type="text" class="search-query span6" placeholder="Quick filter" ng-model="subAccountFilterText">
-				<strong class='pull-right'>Displaying {{currentSubAccountsFiltered.length}} of {{currentSubAccounts.length}} result/s.</strong>
-				<br>
-				</p>
-				<h3>Sub Accounts</h3>
-				<table class="table table-hover table-bordered">
-					<thead>
-						<th>Username</th>
-						<th>Password</th>
-						<th>Action</th>
-					</thead>
-					<tbody>
-						<tr ng-show="currentSubAccounts.length === 0">
-							<td colspan="3">
-								No record found
-							</td>
-						</tr>
-						<tr  ng-repeat="(key, value) in currentSubAccountsFiltered = (currentSubAccounts | filter:subAccountFilterText)" ng-show="currentSubAccounts.length !== 0">
-							<td>{{value.username}}</td>
-							<td>{{value.password}}</td>
-							<td>
-								<button type="button" class="btn btn-link" ng-click="indexCtrl.deleteSubAccount(key,value)">
-									delete
-								</button>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<input type="text" class="form-control input-block" ng-model="new_sub_account_username">
-							</td>
-							<td>
-								<input type="text" class="form-control" ng-model="new_sub_account_password">
-							</td>
-							<td>
-								<div class="btn-group">
-									<button ng-disabled="(!new_sub_account_username || new_sub_account_username.length == 0 ) || (!new_sub_account_password || new_sub_account_password.length == 0)" type="button" class="btn btn-default" ng-click="indexCtrl.registerSubAccount()">
-										{{add_button_text}}
+				<div ng-show="selectedMainAccountKey !== null">
+					<div class="well">
+						<h3>
+							<small>*You can click the link to copy to Clipboard</small> <br>
+							Main Account : 
+							<button title="click to copy to clipboard" clipboard text="selectedMainAccount.username" type='button' class='btn btn-link'>
+								{{selectedMainAccount.username}}
+							</button>
+							<button title="click to copy to clipboard" clipboard text="selectedMainAccount.password" type='button' class='btn btn-link'>
+								{{selectedMainAccount.password}}
+							</button>
+							
+						</h3>
+					</div>
+					<p>
+	                <input type="text" class="search-query span6" placeholder="Quick filter" ng-model="subAccountFilterText">
+					<strong class='pull-right'>Displaying {{currentSubAccountsFiltered.length}} of {{currentSubAccounts.length}} result/s.</strong>
+					<br>
+					</p>
+					<h3>Sub Accounts</h3>
+					<table class="table table-hover table-bordered">
+						<thead>
+							<th>Username</th>
+							<th>Password</th>
+							<th>Action</th>
+						</thead>
+						<tbody>
+							<tr ng-show="currentSubAccounts.length === 0">
+								<td colspan="3">
+									No record found
+								</td>
+							</tr>
+							<tr  ng-repeat="(key, value) in currentSubAccountsFiltered = (currentSubAccounts | filter:subAccountFilterText)" ng-show="currentSubAccounts.length !== 0">
+								<td>{{value.username}}</td>
+								<td>{{value.password}}</td>
+								<td>
+									<button type="button" class="btn btn-link" ng-click="indexCtrl.deleteSubAccount(key,value)">
+										delete
 									</button>
-									<button  type="button" class="btn btn-default" ng-click="indexCtrl.registerBulkSubAccount()">
-										Bulk Sub User
-									</button>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<input type="text" class="form-control input-block" ng-model="new_sub_account_username">
+								</td>
+								<td>
+									<input type="text" class="form-control" ng-model="new_sub_account_password">
+								</td>
+								<td>
+									<div class="btn-group">
+										<button ng-disabled="(!new_sub_account_username || new_sub_account_username.length == 0 ) || (!new_sub_account_password || new_sub_account_password.length == 0)" type="button" class="btn btn-default" ng-click="indexCtrl.registerSubAccount()">
+											{{add_button_text}}
+										</button>
+										<button  type="button" class="btn btn-default" ng-click="indexCtrl.registerBulkSubAccount()">
+											Bulk Sub User
+										</button>
 
-									<button type="button" class="btn btn-default" ng-click="indexCtrl.generateRandomSubAccount(selectedMainAccount)">
-										Generate Sub User
-									</button>
-								</div>								
-							</td>
-						</tr>
-					</tbody>
-				</table>
+										<button type="button" class="btn btn-default" ng-click="indexCtrl.generateRandomSubAccount(selectedMainAccount)">
+											Generate Sub User
+										</button>
+									</div>								
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
 			</div>
 		</div>
 		<div class="clearfix"></div>
