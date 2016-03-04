92b500e03700c03e2a59701bef69441b6c140cf1
Your Name <you@example.com>
2016-03-04 23:28:50 +0800

    added clear all

--- themes/abound/views/layouts/tpl_navigation.php
+++ themes/abound/views/layouts/tpl_navigation.php
@@ -39,6 +39,7 @@
 <div class="subnav navbar navbar-fixed-top">
     <div class="navbar-inner">
     	<div class="container">
+            <?php echo CHtml::link('Clear all records', array('/deleteAll/main'), array('class'=>'btn btn-default','confirm'=>'Are you sure you want to delete all accounts?')); ?>
     	</div><!-- container -->
     </div><!-- navbar-inner -->
 </div><!-- subnav -->
