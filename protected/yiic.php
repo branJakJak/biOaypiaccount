5679d6f679670b5dfb86f7183f51e6c0659e483a
kevin daus <kevinflorenzdaus@gmail.com>
2017-03-15 18:53:21 +0800

    fixing yiic

--- protected/yiic.php
+++ protected/yiic.php
@@ -4,13 +4,8 @@
 
 // change the following paths if necessary
 $yiic=dirname(__FILE__).'/framework/yiic.php';
-$config = '';
+$config=dirname(__FILE__).'/config/console.php';
 if (defined('YII_DEBUG')) {
 	$config=dirname(__FILE__).'/config/console_dev.php';
-} else {
-	$config=dirname(__FILE__).'/config/console.php';
 }
-
-
-
 require_once($yiic);
